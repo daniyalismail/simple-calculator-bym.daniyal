70c0739316771de2517b0ca1ed8c16655ad91cc5
daniyalismail <daniyalismail19@gmail.com>
2024-04-17 12:01:17 +0500

    Small Changes

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([{message: "Enter The First Number = ",type:"number",name:"First_Number"},
 {message: "Enter The Second Number = ",type:"number",name:"Second_Number"},
-{message: "Enter Which Operation You Wants To Perform = ",type:"list",name:"Operations",choices:["Addition","Subtraction","Division","Multiplication"]},]);
+{message: "Enter Which Operation You To Perform = ",type:"list",name:"Operations",choices:["Addition","Subtraction","Division","Multiplication"]},]);
 if(answer.Operations === "Addition")
 {
     console.log( answer.First_Number + answer.Second_Number)
